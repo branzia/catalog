7cfdd1a1c5b06941dc2e136443cb2df02b037082
Branzia <visvanathan.tech@gmail.com>
2025-07-10 09:52:18 +0530

    Adding Configurations through config file

--- config/catalog.php
+++ config/catalog.php
@@ -1,4 +1,21 @@
 <?php 
 
 return [
+
+    'configuration' => [
+        'tab' => 'Catalog',
+        'section' => 'Category',
+        'group' => 'Storefront',
+        'fields' => [
+            'mode' => [
+                'label' => 'Mode',
+                'type' => 'Select',
+                'value' => [
+                    'grid',
+                    'list'
+                ],
+                'default' => 'list',
+            ],
+        ],
+    ]
 ];
